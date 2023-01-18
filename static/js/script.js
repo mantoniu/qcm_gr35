ca6f0cbadea08bfa06d9305221e75caf02b4b5c5
Antoniu2a <91831264+Antoniu2a@users.noreply.github.com>
2023-01-18 17:00:04 +0100

    maj script et card

--- static/js/script.js
+++ static/js/script.js
@@ -17,35 +17,35 @@ function connection(){
 function new_card(){
     document.getElementById('button-add-card').style.display = "none";
     html = ' \
-    <form action="/newcard" class="form-card"> \
+    <form id="new-question" action="/newcard" class="form-card"> \
         <div class="card"> \
             <div class="header-card"> \
                 <textarea type="text" spellcheck="false" placeholder="Ecrire l énoncé..." id="enonce"></textarea> \
             </div> \
             <div class="body-card"> \
                 <div class="content" id="content-response"> \
-                    <div class="list-response"> \
-                        <div class="response" id="response3"> \
+                    <div class="list-response" id="list-response"> \
+                        <div class="response"> \
                             <div class="switch"> \
-                                <input type="checkbox" id="switch3"> \
-                                <label for="switch3"></label> \
+                                <input type="checkbox" name="switch1" id="switch1"> \
+                                <label for="switch1"></label> \
                             </div> \
                             <textarea type="text" placeholder="Ecrire une réponse..." class="textarea-response"></textarea> \
-                            <button onclick="deleteAnswer()" class="trash-button"><i class="fas fa-trash"></i></button> \
+                            <button type="button" onclick="delete_answer(this)" class="trash-button"><i class="fas fa-trash"></i></button> \
                         </div> \
-                        <div class="response" id="response3"> \
+                        <div class="response"> \
                             <div class="switch"> \
-                                <input type="checkbox" id="switch3"> \
-                                <label for="switch3"></label> \
+                                <input type="checkbox" id="switch2"> \
+                                <label for="switch2"></label> \
                             </div> \
                             <textarea type="text" placeholder="Ecrire une réponse..." class="textarea-response"></textarea> \
-                            <button class="trash-button"><i class="fas fa-trash"></i></button> \
+                            <button type="button" onclick="delete_answer(this)" class="trash-button"><i class="fas fa-trash"></i></button> \
                         </div> \
                 </div> \
             </div> \
                 <div class="card-navbar"> \
                     <button class="animation" type="submit" id="apercu">Aperçu</button> \
-                    <div class="circle animation"><i class="fas fa-plus"></i></div> \
+                    <div onclick="add_answer()" class="circle animation"><i class="fas fa-plus"></i></div> \
                     <button class="animation" type="submit" id="ajouter">Valider</button> \
                 </div> \
             </div> \
@@ -54,18 +54,29 @@ function new_card(){
     $("body").append(html);
 }
 
+function delete_answer(object){
+    $(object).parent().remove();
+}
+
+var count = 3;
+
+document.getElementById("new-question").addEventListener('submit', functSubmit);
+
+function functSubmit(event) {
+    count = 3;
+}
 
 
 function add_answer(){
     html = ' \
-    <div class="response" id="response3"> \
+    <div class="response"> \
         <div class="switch"> \
-            <input type="checkbox" id="switch3"> \
-            <label for="switch3"></label> \
+            <input type="checkbox" id="switch'+count+'"> \
+            <label for="switch'+count+'"></label> \
         </div> \
         <textarea type="text" placeholder="Ecrire une réponse..." class="textarea-response"></textarea> \
-        <button class="trash-button"><i class="fas fa-trash"></i></button> \
+        <button type="button" onclick="delete_answer(this)" class="trash-button"><i class="fas fa-trash"></i></button>  \
     </div> ';
-
-    $("#content-response").append(html);
+    count ++;
+    $("#list-response").append(html);
 }
