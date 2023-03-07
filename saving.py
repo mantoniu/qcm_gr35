d974823e54715c47632c07ec1d273287ca2ecd47
iTPasta <kilian.dcs@gmail.com>
2023-03-07 11:08:14 +0100

    pnj

--- saving.py
+++ saving.py
@@ -88,7 +88,9 @@ class StudentsData():
     
     def login(self, email: str, password: str) -> bool:
         for users in self.users_array:
+            print(email + " == " + users.email + " ? " + email == users.email)
             if email == users.email:
+                print(password + " == " + users.password + " ? " + password == users.password)
                 if hash(password) == users.password:
                     return True
                 else:
